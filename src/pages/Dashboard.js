d1a17a93e0221b016a03a1513492704ded07366f
jtlaquec <78872016+jtlaquec@users.noreply.github.com>
2023-07-13 22:26:23 -0500

    IMPORTANTE!
    MUY IMPORTANTE

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import imagen from '../ositoperu.jpg';
+import { URL_API } from "../helpers/Config";
+
 const Dashboard = () => {
-    return(
-        <h1>Dashboard Page</h1>
-    )
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch(`${URL_API}/dashboard`);
+            const jsonData = await response.json();
+            setData(jsonData);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    return (
+        <div>
+            <h1>Dashboard Page</h1>
+            
+
+            {data && (
+                <div>
+                    <h2>Pa Graficar</h2>
+                    <p>Productos: {data.productos}</p>
+                    <p>Usuarios: {data.usuarios}</p>
+                    <p>Total Ventas Histórico: {data.totalVentasHistorico}</p>
+                    <p>Total Compras Histórico: {data.totalComprasHistorico}</p>
+                    <p>Total Ventas Mes: {data.totalVentasMes}</p>
+                    <p>Total Compras Mes: {data.totalComprasMes}</p>
+                    <p>Total Ventas Día: {data.totalVentasDia}</p>
+                    <p>Total Compras Día: {data.totalComprasDia}</p>
+                    {data.meses && data.meses.length > 0 && (
+                        <div>
+                            <h3>Grafico de ventas por Meses:</h3>
+                            <ul>
+                                {data.meses.map((item, index) => (
+                                    <li key={index}>{item.Total} - {item.mes}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
+                </div>
+            )}
+            <img src={imagen} alt="Soy Osito Perú" />
+        </div>
+        
+    );
 }
 
 export default Dashboard;
